cc12e0046b4201183a628696dfede76044bdae72
pavankapuluru <pandukap1517@gmail.com>
2025-11-19 12:48:04 +0530

    update file

--- alltasks.js
+++ alltasks.js
@@ -5,50 +5,100 @@ function celsiusToFahrenheit(c) {
 let value = celsiusToFahrenheit(30);
 console.log(value);
 
-let arr = [1, 2, 3, 4, 5];
-let sum = 0;
-for (let index = 0; index < arr.length; index++) {
-  sum += arr[index];
-}
-console.log(sum);
+// celsiusToFahrenheit
 
-let arr1 = [1, 2, 3, 4, 5];
-let sum1 = 0;
-for (let index = 0; index < arr1.length; index++) {
-  sum1 += arr1[index];
-}
-let avg = sum1 / 2;
+let arr = [1, 2, 3, 4, 5];
+const sumOfArray = (arr) => {
+  let sum = 0;
+  arr.forEach((val) => {
+    sum += val;
+  });
+  return sum;
+};
+console.log(sumOfArray(arr));
+
+// Sum of Array
+
+avg = sumOfArray(arr) / arr.length;
 console.log(avg);
 
-let arr2 = [1, 2, 3, 4, 5];
-let max = arr2[0];
-for (let index = 0; index < arr1.length; index++) {
-  if (max < arr2[index]) {
-    max = arr2[index];
+// average of array
+
+let arrTwo = [1, 2, 3, 4, 5];
+let max = arrTwo[0];
+const maxArray = (val) => {
+  arrTwo.forEach((val) => {
+    if (val > max) {
+      max = val;
+    }
+  });
+  return max;
+};
+console.log(maxArray(arrTwo));
+
+// Maximum Number of in Array
+
+let arrThree = [5, 12, 15, 16, 14];
+const evenNum = (even) => {
+  let newArray = [];
+  even.forEach((element) => {
+    if (element % 2 == 0) {
+      newArray.push(element);
+    }
+  });
+  return newArray;
+};
+console.log(evenNum(arrThree));
+
+// Even number in array
+
+let grade = 75;
+const marksGrade = (element) => {
+  if (element >= 85) {
+    return "grade A";
+  } else if (element >= 70) {
+    return "grade B";
+  } else {
+    return "grade c";
   }
-}
-console.log(max);
-
-let arr3 = [5, 12, 15, 16, 14];
-for (let index = 0; index < arr1.length; index++) {
-  if (arr3[index] % 2 == 0) {
-    console.log(arr3[index]);
-  }
-}
-
-let grade = 80;
-
-if (grade >= 85) {
-  console.log("grade A");
-} else if (grade >= 70) {
-  console.log("grade B");
-} else {
-  console.log("grade c");
-}
+};
+console.log(marksGrade(grade));
+// Grade Marks
 
 let name = "Pavan";
 let rev = "";
-for (let a = 0; a < name.length; a++) {
-  rev = name[a] + rev;
-}
-console.log(rev);
+const revString = (element) => {
+  for (let i = element.length - 1; i >= 0; i--) {
+    rev += element[i];
+  }
+  return rev;
+};
+console.log(revString(name));
+
+// Reverse String
+
+let count = 0;
+
+const vowelCount = (str) => {
+  for (let i = 0; i <= str.length; i++) {
+    if (
+      str[i] == "a" ||
+      str[i] == "e" ||
+      str[i] == "i" ||
+      str[i] == "o" ||
+      str[i] == "u" ||
+      str[i] == "A" ||
+      str[i] == "E" ||
+      str[i] == "I" ||
+      str[i] == "O" ||
+      str[i] == "U"
+    ) {
+      count = count + 1;
+    }
+  }
+  return count;
+};
+res = vowelCount("pavan");
+console.log(res);
+
+//Vowel count
